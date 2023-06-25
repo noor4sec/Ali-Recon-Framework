f40faade09c1c470cf25fcae722a31e7c9e91cfb
Noor Mohammad Gagguturi <85073575+noor4sec@users.noreply.github.com>
2023-06-25 14:54:59 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -16,9 +16,9 @@ sudo apt install figlet -y
 #--------------------------------------Installing Go-Lang-------------------------------------------------------------
 #---------------------------------------------------------------------------------------------------------------------
 echo "[+] Installing Go-Lang....." 
-wget https://go.dev/dl/go1.20.3.linux-amd64.tar.gz
-mv go1.20.3.linux-amd64.tar.gz /root/
-rm -rf /usr/local/go && tar -C /usr/local -xzf go1.20.3.linux-amd64.tar.gz
+wget https://go.dev/dl/go1.20.5.linux-amd64.tar.gz
+mv go1.20.5.linux-amd64.tar.gz /root/
+rm -rf /usr/local/go && tar -C /usr/local -xzf go1.20.5.linux-amd64.tar.gz
 export PATH=$PATH:/usr/local/go/bin
 go version
 #---------------------------------------------------------------------------------------------------------------------
